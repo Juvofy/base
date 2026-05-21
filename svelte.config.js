5780ab41e44fcbf1a7e63da83b47519e9118975b
Tomáš Wróbel <mail@tomaswrobel.dev>
2026-05-21 22:14:14 +0200

    1.0.4

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
-import {vitePreprocess, type SvelteConfig} from "@sveltejs/vite-plugin-svelte";
+import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
 
-const config: SvelteConfig = {
+/** @type {import('@sveltejs/vite-plugin-svelte').SvelteConfig} */
+const config = {
 	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
 	// for more information about preprocessors
 	preprocess: vitePreprocess({
