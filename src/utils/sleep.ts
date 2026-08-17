1b0332ef6035df7be27a604a5d993b5d03ecb8e0
Tomáš Wróbel <mail@tomaswrobel.dev>
2026-08-17 21:15:52 +0200

    Fix utilities

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,3 +1,3 @@
-export async function sleep(ms: number) {
-	await new Promise(resolve => setTimeout(resolve, ms));
+export function sleep(ms: number) {
+	return new Promise<void>(resolve => setTimeout(resolve, ms));
 }
